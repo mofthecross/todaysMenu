431790dd31dc19a4a8d3a264d3a6ec8fb3213cd3
mofthecross <mofthecross@gmail.com>
2017-03-03 13:17:43 -0800

    Mount new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,49 @@
 import React from 'react';
 import Header from './Header';
-
+import Inventory from './Inventory';
+import Fish from './Fish'
+import sampleFishes from '../sample-fishes'
 class App extends React.Component {
   constructor() {
-    super()
+    super();
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+    this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
+  }
+
+  addFish(fish) {
+    //update state
+    const fishes = {...this.state.fishes};
+    const timeStamp = Date.now();
+    fishes[`fish-${timeStamp}`] = fish;
+
+    //set state
+    this.setState({ fishes});
+  }
+
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    })
   }
 
   render() {
     return (
       <div className='catch-of-the-day'>
-        <Header tagline='MENU OF THE DAY'/>
+       <div className='menu'>
+        <Header tagline='Fresh Seafood Market'/>
+        <ul className="list-of-fishes">
+         {
+              Object
+          .keys(this.state.fishes)
+          .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
+         }
+        </ul>
+       </div>
+       <Inventory addFish={this.addFish} loadSamples={ this.loadSamples}/>
       </div>
     )
   }
