ab89bb9cc7f7d432737620e76ff44ee9a356bece
mofthecross <mofthecross@gmail.com>
2017-03-03 12:36:43 -0800

    Initialize App component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        App component
+      </div>
+    )
+  }
+}
+
+export default App;
