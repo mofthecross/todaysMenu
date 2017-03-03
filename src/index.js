f0d1cc01df8c2d2b1ca9d20a63984c2c03a4748e
mofthecross <mofthecross@gmail.com>
2017-03-03 12:57:20 -0800

    Implement StorePicker path and component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@ import {render} from 'react-dom';
 import {BrowserRouter, Match, Miss} from 'react-router';
 import './css/style.css';
 import App from './components/App';
+import StorePicker from './components/StorePicker'
 import NotFound from './components/NotFound';
 
 const Root = () => {
   return (
     <BrowserRouter>
       <div>
-        <Match exactly pattern='/' component={App}/>
+        <Match exactly pattern='/' component={StorePicker}/>
+        <Match exactly pattern="/store/:storeId" component={App}/>
         <Miss component={NotFound}/>
       </div>
     </BrowserRouter>
