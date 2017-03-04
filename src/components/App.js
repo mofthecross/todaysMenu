542657a38ce89320858d020c11ab5720be00fe97
mofthecross <mofthecross@gmail.com>
2017-03-03 18:50:54 -0800

    Clean up code;

--- src/components/App.js
+++ src/components/App.js
@@ -82,12 +82,20 @@ class App extends React.Component {
             {
               Object
               .keys(this.state.fishes)
-              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+              .map(key => <Fish
+                key={key}
+                index={key}
+                details={this.state.fishes[key]}
+                addToOrder={this.addToOrder} />)
             }
           </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order}
+
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
           removeFromOrder={this.removeFromOrder}/>
+
         <Inventory
           addFish={this.addFish}
           loadSamples={ this.loadSamples}
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -18,22 +18,45 @@ class Inventory extends React.Component {
     const fish = this.props.fishes[key]
     return (
       <div className="fish-edit" key={key}>
-        <input type="text" name="name" value={fish.name} placeholder="Fish Name"
+
+        <input
+          type="text"
+          name="name"
+          value={fish.name}
+          placeholder="Fish Name"
           onChange={(e)=> this.handleChange(e, key)}/>
-        <input type="text" name="price" value={fish.price} placeholder="Fish price"
+
+        <input
+          type="text"
+          name="price"
+          value={fish.price}
+          placeholder="Fish price"
           onChange={(e)=> this.handleChange(e, key)}/>
-        <select type="text" name="status" value={fish.status}
+
+        <select
+          type="text" name="status"
+          value={fish.status}
           onChange={(e)=> this.handleChange(e, key)}>
+
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
-
         </select>
 
-        <textarea type="text" name="desc" value={fish.desc} placeholder="Fish Desc"
+        <textarea
+          type="text"
+          name="desc"
+          value={fish.desc}
+          placeholder="Fish Desc"
           onChange={(e)=> this.handleChange(e, key)}>
         </textarea>
-        <input type="text" name="image" value={fish.image} placeholder="Fish Image"
+
+        <input
+          type="text"
+          name="image"
+          value={fish.image}
+          placeholder="Fish Image"
           onChange={(e)=> this.handleChange(e, key)}/>
+
         <button onClick={()=> this.props.removeFish(key)}> remove </button>
       </div>
     )
@@ -45,7 +68,7 @@ class Inventory extends React.Component {
         <h2>
           Inventory
         </h2>
-        {Object.keys(this.props.fishes).map(this.renderInventory)}
+        { Object.keys(this.props.fishes).map(this.renderInventory) }
         <AddFishForm addFish={this.props.addFish}/>
         <button onClick={this.props.loadSamples}>load fish</button>
       </div>
