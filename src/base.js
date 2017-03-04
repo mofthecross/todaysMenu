3cb5018433c1c7b9d40f3d3794e8a895bb5b875a
mofthecross <mofthecross@gmail.com>
2017-03-03 22:00:05 -0800

    Add firebase config file

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyD-07zYwcT1mhl2-HcdnJHnEtEJ96Hp3lc",
+  authDomain: "todays-menu-mdlc.firebaseapp.com",
+  databaseURL: "https://todays-menu-mdlc.firebaseio.com"
+});
+
+export default base;
