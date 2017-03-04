a10de6881c132c40dd3eebf8f09043cf79f3adae
mofthecross <mofthecross@gmail.com>
2017-03-03 22:00:49 -0800

    Implement authentication with facebook and github

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import AddFishForm from './AddFishForm'
+import AddFishForm from './AddFishForm';
+import base from '../base';
 
 class Inventory extends React.Component {
   constructor() {
     super();
+    this.state = {
+      uid: null,
+      owner: null
+    }
     this.renderInventory = this.renderInventory.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.renderLogin = this.renderLogin.bind(this);
+    this.authenticate = this.authenticate.bind(this);
+    this.authHandler = this.authHandler.bind(this);
   }
 
   handleChange(e, key) {
     const fish = this.props.fishes[key];
     const updatedFish = {...fish, [e.target.name]: e.target.value}
     this.props.updateFish(key, updatedFish);
+  }
+
+  renderLogin() {
+    return (
+      <nav className="login">
+        <h2>Inventory</h2>
+        <p> sign in to manage your store's inventory</p>
+        <button
+          className="github"
+          onClick={()=> this.authenticate('github')}>Log In With Github</button>
+        <button
+          className="facebook"
+          onClick={()=> this.authenticate('facebook')}>Log In With Facebook</button>
+      </nav>
+    )
+  }
+
+  authenticate(provider) {
+    console.log('authenticating', provider)
+    base.authWithOAuthPopup(provider, this.authHandler)
+  }
+
+  authHandler(err, authData) {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    //grab store info
+    const storeRef = base.database().ref(this.props.storeId);
+
+    storeRef.once('value', (snapshot) => {
+      const data = snapshot.val() || {};
+
+      if (!data.owner) {
+        storeRef.set({
+          owner: authData.user.uid,
+        });
+      }
+
+      this.setState({
+        uid: authData.user.uid,
+        owner: data.owner || authData.user.uid
+      })
+    })
   }
 
   renderInventory(key) {
@@ -63,17 +115,33 @@ class Inventory extends React.Component {
   }
 
   render() {
-    return (
-      <div >
-        <h2>
-          Inventory
-        </h2>
-        { Object.keys(this.props.fishes).map(this.renderInventory) }
-        <AddFishForm addFish={this.props.addFish}/>
-        <button onClick={this.props.loadSamples}>load fish</button>
-      </div>
-    )
-  }
+    const logout = <button>log out </button>
+    //check if they are not loggedin
+    if (!this.state.uid) {
+      return <div>{this.renderLogin()}</div>
+    }
+
+    if (this.state.uid !== this.state.owner) {
+      return (
+        <div>
+          sorry you are not the owner
+          {logout}
+        </div>
+      )
+    }
+      return (
+        <div >
+          <h2>
+            Inventory
+            {logout}
+          </h2>
+          { Object.keys(this.props.fishes).map(this.renderInventory) }
+          <AddFishForm addFish={this.props.addFish}/>
+          <button onClick={this.props.loadSamples}>load fish</button>
+        </div>
+      )
+    }
+
 }
 
 export default Inventory;
