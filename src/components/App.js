71a4e1df686ad3fe0ae94d3e4c88dd794358e7ef
mofthecross <mofthecross@gmail.com>
2017-03-03 18:27:50 -0800

    Implement bi-directional data flow between Inventory and App

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Header from './Header';
 import Inventory from './Inventory';
 import Fish from './Fish'
-import sampleFishes from '../sample-fishes'
+import Order from './Order';
+import sampleFishes from '../sample-fishes';
+import base from '../base';
+
 class App extends React.Component {
   constructor() {
     super();
@@ -12,6 +15,20 @@ class App extends React.Component {
     }
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
+    this.addToOrder = this.addToOrder.bind(this);
+    this.updateFish = this.updateFish.bind(this);
+  }
+
+  componentWillMount() {
+    this.ref = base.syncState(`${this.props.params.storeId}/fishes`
+      , {
+      context: this,
+      state: 'fishes'
+    });
+  }
+
+  componentWillUnmount() {
+    base.removeBinding(this.ref);
   }
 
   addFish(fish) {
@@ -24,26 +41,44 @@ class App extends React.Component {
     this.setState({ fishes});
   }
 
+  updateFish(key, updatedFish) {
+    const fishes = {...this.state.fishes};
+    fishes[key] = updatedFish;
+    this.setState({ fishes });
+  }
+
   loadSamples() {
     this.setState({
       fishes: sampleFishes
     })
   }
 
+ addToOrder(key) {
+  const order = {...this.state.order};
+  order[key] = order[key] +1 || 1;
+  this.setState({ order });
+ }
+
   render() {
     return (
       <div className='catch-of-the-day'>
-       <div className='menu'>
-        <Header tagline='Fresh Seafood Market'/>
-        <ul className="list-of-fishes">
-         {
+        <div className='menu'>
+          <Header tagline='Fresh Seafood Market'/>
+          <ul className="list-of-fishes">
+            {
               Object
-          .keys(this.state.fishes)
-          .map(key => <Fish key={key} details={this.state.fishes[key]}/>)
-         }
-        </ul>
-       </div>
-       <Inventory addFish={this.addFish} loadSamples={ this.loadSamples}/>
+              .keys(this.state.fishes)
+              .map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
+            }
+          </ul>
+        </div>
+        <Order fishes={this.state.fishes} order={this.state.order}/>
+        <Inventory
+          addFish={this.addFish}
+          loadSamples={ this.loadSamples}
+          fishes={this.state.fishes}
+          updateFish={this.updateFish}
+        />
       </div>
     )
   }
